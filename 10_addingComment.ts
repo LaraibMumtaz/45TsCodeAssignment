ac9cfaff677f302ef8dc8383cab793da6383514d
Laraib Mumtaz <laraibmumtaz07@gmail.com>
2024-06-02 22:17:05 +0500

    updated

---
+++ 10_addingComment.ts
@@ -0,0 +1,3 @@
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let FavoriteNo:number=6 //Creating the variable FavoriteNo with let and its type set to number and then storing the value in it. 
+console.log(`My Favriate number is ${FavoriteNo}`)  // this statement print the result. it print the msg with favorate number that store in "FavoriteNo" Variable. 
