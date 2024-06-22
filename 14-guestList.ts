84f396cb203f1934f8f49c169cb3a25eed0cc7e7
Laraib Mumtaz <laraibmumtaz07@gmail.com>
2024-06-22 16:54:12 +0500

    14

---
+++ 14-guestList.ts
@@ -0,0 +1,7 @@
+//Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. 
+//Then use your list to print a message to each person, inviting them to dinner.
+
+let guestlist=['ali','amna','ayesha']
+for (let i = 0; i<guestlist.length;i++){
+    console.log(` Hi ${guestlist[i]}! You’re invited to a cozy dinner at my place this Saturday at 7 PM. Looking forward to sharing a lovely evening with great food and wonderful company!`)
+} 
