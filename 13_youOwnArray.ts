ae23f8e5c0b1504f243aa30ebcb7c3d4d0b1f231
Laraib Mumtaz <laraibmumtaz07@gmail.com>
2024-06-22 16:46:49 +0500

    13

---
+++ 13_youOwnArray.ts
@@ -0,0 +1,9 @@
+//Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, 
+//and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+let listCar:string[]=['BMW','AUDI','FORD','FERRARI']
+console.log(`i would like to own ${listCar[0]} car`)
+console.log(`Unleash the power and elegance of ${listCar[1]} engineering.`)
+console.log(`I can't wait to drive my own ${listCar[2]}, a perfect blend of power and style.`)
+console.log(`I aspire to drive a ${listCar[3]}, where every journey becomes an extraordinary experience.`)
+
