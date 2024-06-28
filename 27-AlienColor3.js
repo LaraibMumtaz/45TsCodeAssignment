2335767643e73c66303c2fd8c142dfda1c7cde4c
Laraib Mumtaz <laraibmumtaz07@gmail.com>
2024-06-29 00:29:45 +0500

    27

---
+++ 27-AlienColor3.js
@@ -0,0 +1,39 @@
+//Version where the alien is green
+let alien_color = 'green';
+// If-else chain to check the alien's color
+if (alien_color === 'green') {
+    console.log(`The player just earned 5 points for shooting the ${alien_color} alien.`);
+}
+else if (alien_color === 'yellow') {
+    console.log("The player just earned 10 points for shooting the yellow alien.");
+}
+else if (alien_color === 'red') {
+    console.log("The player just earned 15 points for shooting the red alien.");
+}
+//Version where the alien is yellow
+// Assign 'yellow' to the alien_color variable
+let alien_color2 = 'yellow';
+// If-else chain to check the alien's color
+if (alien_color2 === 'green') {
+    console.log(`The player just earned 5 points for shooting the green alien.`);
+}
+else if (alien_color2 === 'yellow') {
+    console.log(`The player just earned 10 points for shooting the ${alien_color2} alien.`);
+}
+else if (alien_color2 === 'red') {
+    console.log("The player just earned 15 points for shooting the red alien.");
+}
+//Version where the alien is red
+// Assign 'red' to the alien_color variable
+let alien_color3 = 'red';
+// If-else chain to check the alien's color
+if (alien_color3 === 'green') {
+    console.log("The player just earned 5 points for shooting the green alien.");
+}
+else if (alien_color3 === 'yellow') {
+    console.log("The player just earned 10 points for shooting the yellow alien.");
+}
+else if (alien_color3 === 'red') {
+    console.log(`The player just earned 15 points for shooting the ${alien_color3} alien.`);
+}
+export {};
