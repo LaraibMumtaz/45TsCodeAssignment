42eeaf58e051d88f88af94d694685f7715107932
Laraib Mumtaz <laraibmumtaz07@gmail.com>
2024-06-29 00:17:06 +0500

    25

---
+++ 25-alienCode.js
@@ -0,0 +1,13 @@
+// Assign 'green' to the alien_color variable
+let alien_color = 'green';
+// Check if the alien's color is green
+if (alien_color === 'green') {
+    console.log("The player just earned 5 points.");
+}
+/// Assign 'red' to the alien_color variable
+let alien_color11 = 'red';
+// Check if the alien's color is green
+if (alien_color11 === 'green') {
+    console.log("The player just earned 5 points.");
+}
+export {};
---
+++ 25-alienCode.ts
@@ -0,0 +1,19 @@
+
+// Assign 'green' to the alien_color variable
+let alien_color = 'green';
+
+// Check if the alien's color is green
+if (alien_color === 'green') {
+    console.log("The player just earned 5 points.");
+}
+
+/// Assign 'red' to the alien_color variable
+let alien_color11 = 'red';
+
+// Check if the alien's color is green
+if (alien_color11 === 'green') {
+    console.log("The player just earned 5 points.");
+}
+
+
+
