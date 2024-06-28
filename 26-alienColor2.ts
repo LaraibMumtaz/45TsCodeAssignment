fe828e1384b47ee06632e6836f0f42b2ca9c1384
Laraib Mumtaz <laraibmumtaz07@gmail.com>
2024-06-29 00:21:48 +0500

    26

---
+++ 26-alienColor2.ts
@@ -0,0 +1,20 @@
+// Assign 'green' to the alien_color variable
+let alien_color = 'green';
+
+// If-else chain to check the alien's color
+if (alien_color === 'green') {
+    console.log("The player just earned 5 points for shooting the alien.");
+} else {
+    console.log("The player just earned 10 points.");
+}
+
+//Version where the else block runs
+// Assign 'yellow' to the alien_color variable
+let alien_color2 = 'yellow';
+
+// If-else chain to check the alien's color
+if (alien_color2 === 'green') {
+    console.log("The player just earned 5 points for shooting the alien.");
+} else {
+    console.log("The player just earned 10 points.");
+}
